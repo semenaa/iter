622b7445738c1d3068cabdd7961102df44957b8c
Semenaa <semenaa@yandex.ru>
2023-02-21 17:17:55 +0500

    smth

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 class FlatIterator:
 
-    def __init__(self, list_of_list):
-        ...
+    def __init__(self, list_of_lists):
+        self.list_of_lists = list_of_lists
 
     def __iter__(self):
-        ...
+        self.cursor = 0
+        self.inner_cursor = 0
         return self
 
     def __next__(self):
-        ...
-        return item
+        self.item = self.list_of_lists[self.cursor][self.inner_cursor]
+        if self.inner_cursor < len(self.list_of_lists[self.cursor]):
+            self.inner_cursor += 1
+        else:
+            self.cursor += 1
+            self.inner_cursor = 0
+        if self.cursor < len(self.list_of_lists):
+            return self.item
+        else:
+            raise StopIteration
+
 
 
 def test_1():
@@ -24,8 +34,8 @@ def test_1():
             FlatIterator(list_of_lists_1),
             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
     ):
-
         assert flat_iterator_item == check_item
+    asd = list(FlatIterator(list_of_lists_1))
 
     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
